a36d80a73b1b8d65aa1655c4690710e59a6c2486
Michael Goerz <goerz@stanford.edu>
2022-04-07 02:15:21 -0400

    Update benchmark plots

--- notebooks/BenchmarkOverview.jl
+++ notebooks/BenchmarkOverview.jl
@@ -22,7 +22,7 @@ using Plots
 
 Plots.default(
     linewidth               = 3,
-    size                    = (550, 300),
+    size                    = (950, 700),
     legend                  = :topleft,
     foreground_color_legend = nothing,
     background_color_legend = RGBA(1, 1, 1, 0.8),
@@ -31,23 +31,79 @@ Plots.default(
 
 using CSV
 
-RSS_BASELINE_MB = 713
-
 # +
 function expected_storage(T; N=5)
     bytes_per_complex_number = 16
     steps_per_T = 10
     bytes_per_MB = 1e6
     N_total = N^2 # two transmons
-    return T * steps_per_T * N_total / bytes_per_MB
+    n_targets = 4
+    return n_targets * T * steps_per_T * N_total / bytes_per_MB
 end
 
 expected_storage(800)
 # -
 
-data = CSV.File(datadir("benchmarks", "benchmark_PE_benchmark_times.csv"));
-display(plot(data.T, data.nanosec_per_fg*1e-9, ylabel="runtime (s)", xlabel="gate duration", label="time per grad eval", title="J_T_PE (levels per transmon N=5)"));
-ax_mem = plot(data.T, data.alloc_memory_MB, ylabel="memory (MB)", xlabel="gate duration", label="allocated");
-plot!(ax_mem, data.T, data.rss_memory_MB .- RSS_BASELINE_MB, ylabel="memory (MB)", xlabel="gate duration", label="RSS");
-plot!(ax_mem, data.T, expected_storage.(data.T), label="storage")
-display(ax_mem)
+function show_benchmarks(csv_times, csv_levels; title="")
+
+    data1 = CSV.File(csv_times)
+    ax11 = plot(data1.T, data1.nanosec_per_fg * 1e-9, label="time per grad eval")
+    plot!(ax11; ylabel="runtime (s)", xlabel="gate duration")
+    ax12 = plot(data1.T, data1.rss_memory_MB, label="RSS")
+    plot!(ax12, data1.T, expected_storage.(data1.T), label="storage")
+    plot!(ax12, ylabel="memory (MB)", xlabel="gate duration")
+    ax13 = plot(data1.T, data1.alloc_memory_MB, label="allocated")
+    plot!(ax13; xlabel="gate duration")
+
+    data2 = CSV.File(csv_levels)
+    ax21 = plot(data2.levels, data2.nanosec_per_fg * 1e-9, label="time per grad eval")
+    plot!(ax21; ylabel="runtime (s)", xlabel="number of transmon levels")
+    ax22 = plot(data2.levels, data2.rss_memory_MB, label="RSS")
+    plot!(ax22, data2.levels, expected_storage.(data2.levels), label="storage")
+    plot!(ax22, ylabel="memory (MB)", xlabel="number of transmon levels",)
+    ax23 = plot(data2.levels, data2.alloc_memory_MB, label="allocated")
+    plot!(ax23; xlabel="number of transmon levels")
+
+    plot(ax11, ax12, ax13, ax21, ax22, ax23, layout=6; plot_title=title)
+end
+
+
+# ## PE Optimization
+
+show_benchmarks(
+    datadir("benchmarks", "PE_benchmark_times.csv"),
+    datadir("benchmarks", "PE_benchmark_levels.csv");
+    title="Semi-AD of PE functional"
+)
+
+show_benchmarks(
+    datadir("benchmarks", "PE_benchmark_times_full_ad.csv"),
+    datadir("benchmarks", "PE_benchmark_levels_full_ad.csv");
+    title="Full-AD of PE functional"
+)
+
+# ## Gate Optimization (sqrt_iSWAP)
+
+show_benchmarks(
+    datadir("benchmarks", "SM_benchmark_times.csv"),
+    datadir("benchmarks", "SM_benchmark_levels.csv");
+    title="Direct Optimization of sqrt_iSWAP"
+)
+
+show_benchmarks(
+    datadir("benchmarks", "SM_SemiAD_benchmark_times.csv"),
+    datadir("benchmarks", "SM_SemiAD_benchmark_levels.csv");
+    title="Semi-AD Optimization of sqrt_iSWAP"
+)
+
+show_benchmarks(
+    datadir("benchmarks", "SM_FullAD_benchmark_times.csv"),
+    datadir("benchmarks", "SM_FullAD_benchmark_levels.csv");
+    title="Full-AD of sqrt_iSWAP"
+)
+
+show_benchmarks(
+    datadir("benchmarks", "SM_FullADcheby_benchmark_times.csv"),
+    datadir("benchmarks", "SM_FullADcheby_benchmark_levels.csv");
+    title="Full-AD of sqrt_iSWAP with cheby prop"
+)
